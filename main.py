db98a5a6a44030387f9284daa85b82c157de016e
Rajesh <rcssharma70@gmail.com>
2022-02-18 17:24:45 +0530

    Completed project

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-# This is a sample Python script.
+##################### Extra Hard Starting Project ######################
+from datetime import datetime
+import pandas as pd
+import random
+import smtplib as con
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+MY_EMAIL = "rcssharma70@gmail.com"
+MY_PASSWORD = "Rajesh@123"
 
+today = datetime.now()
+today_tuple = (today.month, today.day)
+data = pd.read_csv("birthdays.csv")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
 
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today_tuple]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    with con.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=birthday_person["email"],
+                            msg=f"Subject:Happy BirthDay\n\n{contents}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
